01ed68398e42745efbf4ebe48f807c051547dd4f
adi <adinaths@gmail.com>
2016-10-02 21:53:40 +0530

    test

--- src/test/java/fun/commands/handler/CreateUserCommandHandlerTest.java
+++ src/test/java/fun/commands/handler/CreateUserCommandHandlerTest.java
@@ -1,8 +1,35 @@
 package fun.commands.handler;
 
-import static org.junit.Assert.*;
+import fun.commands.RegisterUserCommand;
+import fun.domains.events.UserCreatedEvent;
+import fun.domains.model.User;
+import org.axonframework.test.FixtureConfiguration;
+import org.axonframework.test.Fixtures;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Date;
 
 
 public class CreateUserCommandHandlerTest {
 
+    private FixtureConfiguration<User> fixture;
+    private RegisterUserCommandHandler commandHandler;
+
+    @Before
+    public void setUp() throws Exception {
+        fixture = Fixtures.newGivenWhenThenFixture(User.class);
+        commandHandler = new RegisterUserCommandHandler(fixture.getRepository());
+        fixture.registerAnnotatedCommandHandler(commandHandler);
+    }
+
+    @Test
+    public void shouldCreateUser() throws Exception {
+        RegisterUserCommand command = new RegisterUserCommand("userId", "name", new Date());
+        fixture
+                .given(Collections.emptyList())
+                .when(command)
+                .expectEvents(new UserCreatedEvent(command.getId(), command.getUserName(), command.getDateOfBirth()));
+    }
 }
